bb0dab0bad55b735f30aa3d1f21e464730c78f46
Naureen <naureen.imran123@yahoo.com>
2024-01-20 14:41:40 +0000

    practice nested array

--- testConsole/Program.cs
+++ testConsole/Program.cs
@@ -18,8 +18,10 @@ Console.WriteLine("Hello, World!");
       
       //Read and Display Input
       Console.Write("Please enter your name ");
-      string name = Console.ReadLine();
-      Console.WriteLine("Welcome "+ name +"!");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string name = Console.ReadLine();
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+Console.WriteLine("Welcome "+ name +"!");
 
 // Array
 string [] cities= new string[3]{"manchester", "london", "edinburgh"};
@@ -30,4 +32,8 @@ Console.WriteLine("Second city: "+cities[1]);
 // Array in int
 int [] numbers= new int [10]{1,2,3,4,5,6,7,8,9,10};
 Console.WriteLine("Fifth Number in Array : "+ numbers[4]);
-Console.Read();
+
+
+int[,] coord= new int [2, 3] { { -1, 2, 3 }, { 0, 2, 3 } };
+Console.WriteLine("Coordinate(0,0 ): "+ coord[0,0]);
+Console.WriteLine("Coordinate(1,2 ): "+ coord[1,2]);
